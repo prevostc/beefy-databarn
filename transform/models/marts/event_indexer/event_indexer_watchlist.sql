b75f828f3e2ca0703270f60dd7b8405c53c88df4
Clément Prévost <998369+prevostc@users.noreply.github.com>
2024-01-05 13:15:45 +0100

    Use pg as a state backend

--- transform/models/marts/event_indexer/event_indexer_watchlist.sql
+++ transform/models/marts/event_indexer/event_indexer_watchlist.sql
@@ -16,12 +16,18 @@ boosts as (
 
     select * from {{ ref('int_is_active_boosts' ) }}
 
+),
+
+zaps as (
+
+    select * from {{ ref('stg_zaps' ) }}
+
 )
 
 select
     contract_address,
     chain,
-    '{"erc20:transfer"}'::text [] as events
+    '{"IERC20:Transfer"}'::text [] as events
 from vaults
 where is_active
 
@@ -30,6 +36,14 @@ union all
 select
     contract_address,
     chain,
-    '{"erc20:transfer"}'::text [] as events
+    '{"IERC20:Transfer"}'::text [] as events
 from boosts
 where is_active
+
+union all
+
+select
+    router_contract_address as contract_address,
+    chain,
+    '{"BeefyZapRouter:FulfilledOrder"}'::text [] as events
+from zaps
