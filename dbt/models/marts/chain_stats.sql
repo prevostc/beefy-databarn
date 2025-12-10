d2a1c93a9077aba8e9307624dae7704f5d660615
prevostc <998369+prevostc@users.noreply.github.com>
2025-12-10 22:35:31 +0100

    Remove wrong chain stats wrong data

--- dbt/models/marts/chain_stats.sql
+++ dbt/models/marts/chain_stats.sql
@@ -23,6 +23,23 @@
   {% set max_date = '2021-07-31 19:30:00' %}
 {% endif %}
 
+WITH cleaned_tvl_by_chain AS (
+  SELECT
+    network_id,
+    date_time,
+    tvl_usd,
+    vault_tvl_usd,
+    gov_tvl_usd,
+    clm_tvl_usd
+  FROM {{ ref('stg_beefy_db__tvl_by_chain') }}
+
+  -- remove obviously erroneous data
+  WHERE NOT (
+    network_id = 1 AND
+    date_time BETWEEN '2024-04-03' AND '2024-04-04'
+    AND tvl_usd > {{ to_decimal('8000000000') }}
+  )
+)
 SELECT
   c.chain_id,
   c.chain_name,
@@ -33,7 +50,7 @@ SELECT
   argMax(t.vault_tvl_usd, t.date_time) as vault_tvl_usd,
   argMax(t.gov_tvl_usd, t.date_time) as gov_tvl_usd,
   argMax(t.clm_tvl_usd, t.date_time) as clm_tvl_usd
-FROM {{ ref('stg_beefy_db__tvl_by_chain') }} t
+FROM cleaned_tvl_by_chain t
 INNER JOIN {{ ref('chain') }} c
   ON t.network_id = c.chain_id
 {% if is_incremental() %}
